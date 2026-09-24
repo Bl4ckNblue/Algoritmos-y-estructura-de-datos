b18e01b06c4b0bf0bf700dd2583544f1c49cc0bf
Bl4ckNblue <137901620+Bl4ckNblue@users.noreply.github.com>
2026-09-24 09:51:00 -0500

    Refactor methods for linked list operations

--- src/LES.java
+++ src/LES.java
@@ -1,43 +1,108 @@
 public class LES {
-    private Nodo inicio, nuevo, p, q;
-
-    public void insertarxInicio (int dato){
+    private Nodo inicio,nuevo,p,q; 
+    
+    public void insertarxInicio(int dato){
         nuevo = new Nodo(dato);
-        if (inicio == null) {
+        if(inicio == null)
             inicio = nuevo;
-        }else{
+        else{
             nuevo.setSgte(inicio);
             inicio = nuevo;
         }
     }
     public void eliminaxInicio(){
-        if (inicio != null){
+        if(inicio != null){
             inicio = inicio.getSgte();
         }
     }
-    public String muestraLE() {
-        String cadena = "";
-        if (inicio != null){
+    public String muestraLE(){
+        String cadena="";
+        if(inicio != null){
             p = inicio;
-            while (p!= null){
-                cadena += p.getDato() + " - ";
+            while (p!=null) {
+                cadena += p.getDato()+" - ";
                 p = p.getSgte();
             }
+            //cadena += p.getDato()+" - ";
         }
         return cadena;
     }
-    public void eliminarxFinal() {
-         if(inicio != null){
-             if(inicio.getSgte() == null){
-                inicio = null;
-            }else{
-              p = inicio; q = inicio;
-              while(p.getSgte() != null ){
-                 q=p; p=p.getSgte();
-              }
-              q.getSgte(null);
-             }
-          }
+
+    public void insertarxFinal(int dato) {
+        nuevo = new Nodo(dato);
+        if (inicio == null) {
+            inicio = nuevo;
+        } else {
+            p = inicio;
+            while (p.getSgte() != null) {
+                p = p.getSgte();
+            }
+            p.setSgte(nuevo);
+
         }
     }
-}
+        public void eliminaxFinal(){
+            if(inicio!= null) {
+                if (inicio.getSgte() == null) {
+                    inicio = null;
+                }
+            }else{
+                    p=inicio; q=inicio;
+                    while(p.getSgte() != null){
+                        q = p;
+                        p = p.getSgte();
+                    }
+                    q.setSgte(null);
+                }
+        }
+
+        public void insertaDespues(int dato, int posicion){
+            nuevo = new Nodo(dato);
+            if(inicio == null) {
+                inicio = nuevo;
+            } else{
+                p = inicio;
+                while(p != null && p.getDato() != posicion) {
+                    p = p.getSgte();
+                    if (p.getDato() == posicion) {
+                        nuevo.setSgte(p.getSgte());
+                        p.setSgte(nuevo);
+                    }
+                }
+            }
+        }
+        public void insertaAntes(int dato, int posicion){
+            nuevo = new Nodo(dato);
+            if(inicio == null) {
+                inicio = nuevo;
+            }else{
+                p = inicio; q = inicio;
+                while(p != null && p.getDato() != posicion){
+                    q= p;   p = p.getSgte();
+                }
+                if(p.getDato() == posicion){
+                    nuevo.setSgte(p);  q.setSgte(nuevo);
+                }
+            }
+        }
+
+        public void  eliminaEntre(int posicion) {
+            if (inicio != null) {
+                if (inicio.getSgte() == null)
+                    inicio = null;
+                else {
+                    p = inicio;
+                    q = inicio;
+                    while (p.getSgte() != null && p.getDato() != posicion) {
+                        q = p;
+                        p = p.getSgte();
+                    }
+                    if (p.getDato() == posicion)
+                        q.setSgte(p.getSgte());
+
+
+                }
+            }
+        }
+
+    }
